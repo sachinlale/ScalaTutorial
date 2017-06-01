d2faa8c5bfd712c7d5e8fa24648801e7c3d292de
sachin <sachinlale@gmail.com>
2017-06-01 17:15:28 +0530

    fix

--- scala-slick-poc/app/models/Models.scala
+++ scala-slick-poc/app/models/Models.scala
@@ -1,20 +1,4 @@
 package models
 
-import models.Department.Department
-
-/*
- * One Approach but cannot use in case pattern matching directly as used in below Employee.
- * WIth this it has to be use Department.Value
- * 
- * 
- */
- object Department extends Enumeration  {
-  type Department = Value
-  val SALES = withName("Sales") 
-  val HR = withName("Human Resource") 
-  val ADMIN = withName("Admin") 
-  val ENGINEERING = withName("Engineering")
-}
-
 case class Employee(name: String, email: String, department: String, position:String, id: Option[Int]=None)
 
